cf3f4b799de06d0757e6f2f65653be8fdaadf2f1
NeuroByte79 <sk3419407@gmail.com>
2026-05-15 21:43:50 +0530

    Added day03_operators.py file

---
+++ FUNDAMENTALS/day03_operators.py
@@ -0,0 +1,52 @@
+# ++++++-------> OPERATORS
+#@@@@@---------> An operators in python is special symbol or keywords used to perform operation on variables and values.
+
+
+# @@@@@@++++ python categorized operators into several groups 
+# 1. ARITHMETICS OPERATORS
+""" (i). Addition (+)  ---> A + B
+    (ii). Subtraction (-) ---> A - B
+    (iii). Multiplication (*) ----> A * B
+    (iv). Division (/) -----> A / B
+    (v). Floor Division (//) ----> A // B
+    (vi). Modulus (%) ------> A % B
+    (Vii). Exponential (**) -----> A ** B
+"""
+print("Addition : ", 23 + 45,"\n")
+print("Subtraction : ", 45 - 15,"\n")
+print("Multiplication : ", 12 * 4,"\n")
+print("Division : ", 124 / 9,'\n')
+print("Modulus : ", 15 % 2)
+print("Floor Division : ", 125//7,'\n')
+print("Exponential : ", 12 ** 2,'\n')
+
+# Floating point numbers
+print("Floating Number PI : ", 3.14,'\n')
+print("Floating Number Gravity : ",9.81,'\n')
+
+# Complex numbers 
+
+print("Complex Number : ", 3 - 9j,'\n')
+print("Multiplication of Complex NUmbers : ", (3 - 4j) * (7 - 8j),'\n')
+
+# Declaring variables at the top first 
+a  = 19 
+b = 7
+Total = a + b 
+Diff = a - b 
+Product = a * b
+Division = a / b
+Reminder = a % b
+Floor_division = a // b
+Exponential = a ** b
+
+print("a + b : ",Total,"\n")
+print("a - b : ",Diff,'\n')
+print("a * b : ",Product,'\n')
+print("a / b : ", Division,'\n')
+print("a % b : ",Reminder,'\n')
+print("a // b : ", Floor_division,'\n')
+print("a ** b : ", Exponential,'\n')
+ 
+
+
